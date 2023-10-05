f1af3d5e95fac0bea7d86efcb6858b822ac5d7cc
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-05 14:29:29 +0200

    Add project files.

---
+++ 12 Loop/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Num As Integer
+        For Num = 1 To 5
+
+            Num += 1
+            MsgBox(Num)
+
+            If Num = 4 Then
+                Exit For
+
+            End If
+        Next
+        MsgBox("out of the loop")
+    End Sub
+End Class
